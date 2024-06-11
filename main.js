45740c1e33d6f02c7c4aecb8cc368292d847f3d8
Marlier eymeric <141547366+eymeric57@users.noreply.github.com>
2024-06-11 21:14:50 +0200

    modif chemin

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GLTFLoader } from '../node_modules/three/addons/loaders/GLTFLoader.js';
 
 // Initialisation de la scène, caméra et renderer
 let canvas = document.querySelector('#c');
